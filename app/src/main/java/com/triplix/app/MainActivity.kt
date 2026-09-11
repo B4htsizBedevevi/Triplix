4fabcfeac284a322e26049b04f6a3cd6ac3d0580
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-11 23:15:17 +0300

    refactor: reset app entry point to clean skeleton

--- app/src/main/java/com/triplix/app/MainActivity.kt
+++ app/src/main/java/com/triplix/app/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { TriplixV8App() }
+        setContent {
+            TriplixApp()
+        }
     }
 }

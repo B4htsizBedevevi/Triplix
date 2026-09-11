5d1ba49072a985b087e40c9868faf060e5dc34ab
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-11 22:37:28 +0300

    feat: switch app entry to polished V6

--- app/src/main/java/com/triplix/app/MainActivity.kt
+++ app/src/main/java/com/triplix/app/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { TriplixV5App() }
+        setContent { TriplixV6App() }
     }
 }

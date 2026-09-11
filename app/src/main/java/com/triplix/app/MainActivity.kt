b4ff601d8fbe7d4e11edfd4ea0bcec297008b3f9
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-11 22:54:46 +0300

    chore: switch TRIPLIX launcher to V7

--- app/src/main/java/com/triplix/app/MainActivity.kt
+++ app/src/main/java/com/triplix/app/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { TriplixV6App() }
+        setContent { TriplixV7App() }
     }
 }
